040691f4f3c77b52300fc346e435deb457c357a7
sagarpujar <50769992+SAGARPUJAR@users.noreply.github.com>
2024-09-06 21:57:52 +0530

    Created the Mapper Profile to Convert the POCO Class to Product Entity Class.

---
+++ ApiTest.Contracts/Mapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using ApiTest.Contracts.Model;
+using ApiTest.Entity.Entites;
+using AutoMapper;
+
+namespace ApiTest.Contracts.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Create a map from ProductPoco to Product
+            CreateMap<ProductPoco, Product>();
+
+            // If you need to map the other way as well:
+            CreateMap<Product, ProductPoco>();
+        }
+    }
+}
