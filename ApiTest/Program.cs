596d618bddbffd954f9b7b6b8bd53c12455e2ebf
sagarpujar <50769992+SAGARPUJAR@users.noreply.github.com>
2024-09-06 22:05:49 +0530

    Configured the Configurations, Registered the AppDbContext, Added Dependency Injection for IProductRepository and IProductContracts and also added the AutoMapper Configurations.

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,12 +1,19 @@
+using ApiTest.Contracts;
+using ApiTest.Contracts.Mapper;
+using ApiTest.Entity;
+using ApiTest.Entity.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDBContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductContracts, ProductContracts>();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
