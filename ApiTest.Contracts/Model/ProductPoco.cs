8ddb5a6d17f8bf4ee27eb0a57a6ae3dd69900397
sagarpujar <50769992+SAGARPUJAR@users.noreply.github.com>
2024-09-06 21:55:05 +0530

    Created the Product POCO Class to pass between Controller and Contracts

---
+++ ApiTest.Contracts/Model/ProductPoco.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ApiTest.Contracts.Model
+{
+    public class ProductPoco
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ProductDescription { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+        [JsonIgnore]
+        public DateTime CreatedOn { get; set; }
+        [JsonIgnore]
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
